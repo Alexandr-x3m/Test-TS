f878fe94822f1717469e97322328c53c7e2f21ce
Alexandr <60668244+Alexandr-x3m@users.noreply.github.com>
2021-04-12 13:48:38 +0300

    Update generic.ts

--- generic.ts
+++ generic.ts
@@ -8,23 +8,22 @@ interface Objecta {
   n: string
 }
 
+let groupBy = <T, U>(array: null | T[], callback: (el: T) => U | typeof NaN | null ): null | {[key: string]: T[] | typeof NaN[]}  => {
 
-let groupBy = <T, U>(array: T[], callback: (el: T) => U ): {[key: string]: T[] }  => {
+    if (array === null) return null
 
     let result: { [key: string]: T[] } = {}
     
-    array.map((el: T) => {
-        let floor = callback(el)
-        let key = String(floor)
-        
-
-        //we are check if current key doesn't exist in this object
-        if (typeof result[key] === 'undefined') {
-            return result[key] = [el]
-            
-        }
-        return result[key].push(el)
-    })
+      array.map((el: T) => {
+          let floor = callback(el)
+          let key = String(floor)
+          
+          //we are check if current key doesn't exist in this object
+          if (typeof result[key] === 'undefined') {
+              return result[key] = [el]   
+          }
+          return result[key].push(el)
+      })
   console.log(result)
   console.log('-------------------------')
   return result
